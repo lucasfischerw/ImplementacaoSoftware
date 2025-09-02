bf21ab97955cb46a173a092894b901175c5abcfd
Lucas Fischer Wachholz <lucasfischerw@gmail.com>
2025-09-01 22:06:42 -0300

    Banco

--- app/Hello/routes.py
+++ app/Hello/routes.py
@@ -1,12 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+from ..models import User, db
 
 hello_bp = Blueprint('Hello', __name__)
 name_bp = Blueprint('HelloNome', __name__)
 
 @hello_bp.route('/')
 def index():
-    return "<div style='display:grid;place-content:center;background-color:orange;color:white;width: 100vw;height:100vh;'><h1 style='font-weight:bold;text-align:center;'>Olá mundo!</h1></br><a href='/sobre' style='background-color: white;padding: 10px;color:black;border-radius: 15px;text-align-center;'>Acessar página sobre</a>"
+    usuarios = User.query.all()
+    return render_template('index.html', usuarios=usuarios)
+
 
 @name_bp.route('/sobre')
 def index():
-    return "<div style='display:grid;place-content:center;background-color:red;color:white;width: 100vw;height:100vh;'><h1 style='font-weight:bold;text-align:center;'>Olá, Lucas Fischer!</h1></br><a style='background-color: white;padding: 10px;color:black;border-radius: 15px; text-align-center;' href='/'>Voltar à página inicial</a>"
+    return render_template('sobre.html')
--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,14 @@
 from flask import *
 from .Hello.routes import hello_bp, name_bp
+from .models import db, User
 
 def create_app():
     app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hello.db'
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
 
     app.register_blueprint(hello_bp)
     app.register_blueprint(name_bp)
